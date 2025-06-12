3d1eb1cedc2d696ba61ff97ca432e90925c179e9
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-06-12 22:47:23 +0200

    test

---
+++ Src/Test.py
@@ -0,0 +1,29 @@
+import torch
+import os
+from huggingface_hub import login
+os.environ["HF_TOKEN"] = "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"
+os.environ['HF_HOME'] = 'modle'
+token = os.environ["HF_TOKEN"]
+
+import torch
+from diffusers.utils import load_image
+from diffusers.pipelines.flux.pipeline_flux_controlnet import FluxControlNetPipeline
+from diffusers.models.controlnet_flux import FluxControlNetModel
+
+login(token='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', add_to_git_credential=True)
+base_model = 'black-forest-labs/FLUX.1-dev'
+controlnet_model = 'InstantX/FLUX.1-dev-Controlnet-Canny'
+controlnet = FluxControlNetModel.from_pretrained(controlnet_model, torch_dtype=torch.bfloat16)
+pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
+pipe.to("cuda")
+
+control_image = load_image("https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/resolve/main/canny.jpg")
+prompt = "A girl in city, 25 years old, cool, futuristic"
+image = pipe(
+    prompt,
+    control_image=control_image,
+    controlnet_conditioning_scale=0.6,
+    num_inference_steps=1,
+    guidance_scale=3.5,
+).images[0]
+image.save("image.jpg")
---
+++ Src/ratto.py
@@ -0,0 +1,47 @@
+from huggingface_hub import login
+
+import os
+import subprocess
+os.environ["HF_TOKEN"] = "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"
+os.environ['HF_HOME'] = 'modle'
+token = os.environ["HF_TOKEN"]
+
+login(token="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")
+def main():
+    # Percorsi dataset e output
+    output_dir = "modle"
+
+    # Nome base modello
+    pretrained_model = "black-forest-labs/FLUX.1-dev"
+    controlnet_pretrained = 'InstantX/FLUX.1-dev-Controlnet-Canny'
+    # Script ufficiale diffusers per il training
+    training_script = "train_controlnet_flux.py"
+        # Comando per chiamare lo script di training
+    command = [
+        "accelerate", "launch", training_script,
+        "--pretrained_model_name_or_path", pretrained_model,
+        "--controlnet_model_name_or_path", controlnet_pretrained,
+        "--output_dir", output_dir,
+        "--dataset_name", "controlnet_dataset",
+        "--conditioning_image_column", "conditioning_image",
+        "--image_column", "image",
+        "--caption_column", "prompt",
+        "--resolution", "512",
+        "--train_batch_size", "1",
+        "--learning_rate", "1e-5",
+        "--max_train_steps", "3",
+        "--checkpointing_steps", "2",
+        "--validation_steps", "2",
+        "--mixed_precision" , "bf16",
+        "--validation_image", "controlnet_dataset/sample_0000.jpg",
+        "--validation_prompt", "red circle with blue background cyan circle with brown floral background",
+        "--gradient_accumulation_steps", "1 ",
+    ]
+
+    print("Esecuzione comando Accelerate:")
+    print(" ".join(command))
+
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    main()
