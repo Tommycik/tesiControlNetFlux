cff58e76efb923648645b192ac95f73821dbc6f2
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-06-17 19:41:48 +0200

    jsonl

--- Src/controlnet_dataset/jsonConverter.py
+++ Src/controlnet_dataset/jsonConverter.py
@@ -1,9 +1,14 @@
 import json
-
+import os
 
 with open("dataset.json", "r") as f:
     data = json.load(f)
 
+for item in data:
+    if "image" in item:
+        item["image"] = os.path.join("controlnet_dataset", item["image"])
+    if "condition_image" in item:
+        item["condition_image"] = os.path.join("controlnet_dataset", item["condition_image"])
 
 with open("dataset.jsonl", "w") as f:
     for item in data:
