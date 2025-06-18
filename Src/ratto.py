708c80fecd16db0820abb41a5f45d65c937e0611
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-06-18 18:32:35 +0200

    Update ratto.py

--- Src/ratto.py
+++ Src/ratto.py
@@ -2,7 +2,7 @@ from huggingface_hub import login
 from datasets import load_dataset
 import os
 import subprocess
-os.environ["HF_TOKEN"] = "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"
+os.environ["HF_TOKEN"] = "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"
 token = os.environ["HF_TOKEN"]
 
 login(token)
