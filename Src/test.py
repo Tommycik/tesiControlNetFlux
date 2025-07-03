34f042c4f01e2584b389fbe1ee363fee51e24f2f
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-07-03 16:37:57 +0200

    Update test.py

--- Src/test.py
+++ Src/test.py
@@ -10,12 +10,12 @@ from diffusers.models.controlnets.controlnet_flux import FluxControlNetModel
 user_input = input("Enter token: ")
 login(token = user_input)
 base_model = 'black-forest-labs/FLUX.1-dev'
-controlnet_model = 'tommycik/controlFlux'
+controlnet_model = 'tommycik/controlFluxAlcol'
 controlnet = FluxControlNetModel.from_pretrained(controlnet_model, torch_dtype=torch.bfloat16)
 pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
 pipe.to("cuda")
 
-control_image = load_image("https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/resolve/main/canny.jpg")
+#control_image = load_image("https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Canny/resolve/main/canny.jpg")
 user_input = input("Enter prompt: ")
 #prompt = "A tall glass with gemstones"
 prompt = user_input
