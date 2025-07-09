a945ec9e1c095a618495489f1e18df5a8e5375e6
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-07-09 13:31:01 +0200

    control lora

--- Src/trainingLora.py
+++ Src/trainingLora.py
@@ -30,10 +30,10 @@ def main():
         "--mixed_precision", "bf16",
         "--validation_image", "controlnet_dataset/images/sample_0000.jpg",
         "--validation_prompt", "transparent glass on white background, the bottom part of the glass presents light grooves",
-        "--train_batch_size", "4",#for speed,if 8 and 1 gradient needs more than 80 gb of ram(79 + 144 mb)
-        "--gradient_accumulation_steps", "2",
+        "--train_batch_size", "2",#for speed,if 8 and 1 gradient needs more than 80 gb of ram(79 + 144 mb)
+        "--gradient_accumulation_steps", "4",
         "--gradient_checkpointing",#if not it goes out of memory if train batch >2
-        "--enable_model_cpu_offload",
+        #"--enable_model_cpu_offload",
         "--use_8bit_adam",
         "--set_grads_to_none",
 
