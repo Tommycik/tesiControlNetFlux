1412d532a463b1caa9fbfcc8ab1c86511396f33a
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-07-14 01:58:13 +0200

    lora reduced

--- Src/testLoraReduced.py
+++ Src/testLoraReduced.py
@@ -1,8 +1,6 @@
 import torch
 from huggingface_hub import login
 from diffusers.utils import load_image
-import torch
-from diffusers import FluxControlPipeline
 from diffusers.pipelines.flux.pipeline_flux_controlnet import FluxControlNetPipeline
 from diffusers.models.controlnets.controlnet_flux import FluxControlNetModel
 from peft import PeftModel  # You need to have peft installed: pip install peft
@@ -17,14 +15,14 @@ lora_weights_repo = "tommycik/controlFluxAlcol-LoRAReduced"
 base_flux_model = "black-forest-labs/FLUX.1-dev"
 
 # Load base ControlNet model
-#controlnet = FluxControlNetModel.from_pretrained(base_controlnet_model, torch_dtype=torch.bfloat16)
+controlnet = FluxControlNetModel.from_pretrained(base_controlnet_model, torch_dtype=torch.bfloat16)
 
 # Load LoRA weights and merge into base controlnet
-#controlnet = PeftModel.from_pretrained(PeftModel.from_pretrained(controlnet, lora_weights_repo + "/controlnet_lora"))
+controlnet = PeftModel.from_pretrained(PeftModel.from_pretrained(controlnet, lora_weights_repo + "/controlnet_lora"))
 
 # Load the pipeline with the adapted ControlNet
-pipe = FluxControlPipeline.from_pretrained(base_controlnet_model, torch_dtype=torch.float16).to("cuda")
-pipe.load_lora_weights(lora_weights_repo)
+pipe = FluxControlNetPipeline.from_pretrained(base_flux_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
+pipe.to("cuda")
 
 # Load control image
 control_image = load_image("controlnet_dataset/imagesControlCanny/sample_0000_canny.jpg")
