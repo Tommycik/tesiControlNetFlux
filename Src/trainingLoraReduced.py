3f701f2409d1af49b661e9e636b2a7b3794fd4bc
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-07-13 22:02:55 +0200

    lora reduced

--- Src/trainingLoraReduced.py
+++ Src/trainingLoraReduced.py
@@ -25,7 +25,7 @@ def main():
         "--image_column", "image",
         "--caption_column", "prompt",
         "--jsonl_for_train", "./controlnet_dataset/dataset.jsonl",
-        "--resolution", "256",  # ⬅️ reduced from 512
+        "--resolution", "128",  # ⬅️ reduced from 512
         "--learning_rate", "5e-5",
         "--max_train_steps", "2",
         "--checkpointing_steps", "250",
