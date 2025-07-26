ec51875b51b32bdca8bf43bbf0d3faed28715b03
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-07-26 15:10:26 +0200

    file di inizio

--- Src/testControlNetHed.py
+++ Src/testControlNetHed.py
@@ -28,7 +28,7 @@ controlnet = FluxControlNetModel.from_pretrained(controlnet_model, torch_dtype=t
 pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
 pipe.to("cuda")
 
-control_image = load_image("controlnet_dataset/imagesControlCanny/sample_0000_hed.jpg")
+control_image = load_image("controlnet_dataset/imagesControlHed/sample_0000_hed.jpg")
 def main():
     user_input = input("Enter prompt: ")
     #prompt = "A tall glass with gemstones"
