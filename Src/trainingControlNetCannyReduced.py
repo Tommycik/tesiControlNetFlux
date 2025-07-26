ef76ba092808ee24e8abdc0ac62b000d761e3ee0
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-07-26 17:54:11 +0200

    control hed e a100 canny

--- Src/trainingControlNetCannyReduced.py
+++ Src/trainingControlNetCannyReduced.py
@@ -47,7 +47,7 @@ def main():
         "--hub_model_id", "tommycik/controlFluxAlcolReduced"
     ]
 
-   #4 ore di training con questa configurazione
+   #4 ore di training con questa configurazione su a100
     print("Esecuzione comando Accelerate:")
     print(" ".join(command))
 
