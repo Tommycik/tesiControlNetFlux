477e494c711c5ffbdd3ad57fa460e1bee2ee1806
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-08-28 21:50:15 +0200

    lora reduced

--- Src/scripts/controlnet_infer_api.py
+++ Src/scripts/controlnet_infer_api.py
@@ -55,7 +55,7 @@ pipe.to("cuda")
 controlnet_type_capitalized = args.controlnet_type.capitalize()
 from pathlib import Path
 
-control_img = args.control_image or f"controlnet_dataset/imagesControl{controlnet_type_capitalized}/sample_0000_{args.controlnet_type}.jpg"
+control_img = args.control_image or f"./controlnet_dataset/imagesControl{controlnet_type_capitalized}/sample_0000_{args.controlnet_type}.jpg"
 control_image = load_image(control_img)
 
 result = pipe(
