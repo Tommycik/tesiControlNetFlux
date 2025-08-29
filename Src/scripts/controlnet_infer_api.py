5fae3583cac25ffcf329fbe22690ea89da37e1dd
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-08-29 18:38:37 +0200

    migliorato polling in caso di errore

--- Src/scripts/controlnet_infer_api.py
+++ Src/scripts/controlnet_infer_api.py
@@ -86,7 +86,7 @@ response = cloudinary.uploader.upload(
     folder="repo_image",
     resource_type="image"
 )
-print(response["secure_url"])
+print(response["secure_url"], flush=True)
 
 # Carica immagine di controllo
 control_img_byte_arr = BytesIO()
