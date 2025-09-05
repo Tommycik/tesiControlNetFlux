c23e422a552806eca4264fffd1273bd1a65c74a4
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-09-06 00:14:02 +0200

    pulizia codice, da separare le parti di codice in funzioni più piccole

--- Src/controlnet_dataset/datasetGen.py
+++ Src/controlnet_dataset/datasetGen.py
@@ -12,7 +12,7 @@ prompts_file = Path("content/captions_eng.csv")
 output_dir = Path("")
 output_dir.mkdir(parents=True, exist_ok=True)
 
-# Leggi i prompt
+#Prompt
 with open(prompts_file, "r", encoding="utf-8") as f:
     prompts = [line.strip() for line in f.readlines()]
 
@@ -28,7 +28,6 @@ print("Creating dataset")
 for idx, (img_path, canny_path, prompt) in tqdm(enumerate(zip(image_files, canny_files, prompts)), total=len(prompts)):
     base_name = f"sample_{idx:04d}"
 
-    # Copia o referenzia i file nel dataset
     new_img_path = output_dir / f"images/{base_name}.jpg"
     new_canny_path = output_dir / f"imagesControlCanny/{base_name}_canny.jpg"
 
@@ -95,4 +94,4 @@ with open("dataset_hed.json", "r") as f:
 with open("dataset_hed.jsonl", "w") as f:
     for item in data:
         f.write(json.dumps(item) + "\n")
-print(f"✅ Dataset creato in: {output_dir}")
+print(f"Dataset created in {output_dir}")
